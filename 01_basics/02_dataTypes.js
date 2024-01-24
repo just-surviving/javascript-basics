026037234068737503d6004f4ff1bf5a44feac5d
just-surviving <147724866+just-surviving@users.noreply.github.com>
2024-01-24 22:08:40 +0000

    changes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version of js
+
+//alert() //we are using nodejs , not browser
+
+console.log(3+3)
+
+console.log("Magnus")
+
+let name = "Magnus"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => use in uniqueness
+
+
+console.log(typeof age)
