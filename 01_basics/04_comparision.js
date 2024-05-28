53a2325ff784900884c6b88054fefd392b3178e5
just-surving <22uec002@lnmiit.ac.in>
2024-05-28 15:59:18 +0530

    first commit

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+//console.log("2">1);
+//console.log("02">1); different datatypes
+
+// avoid the below
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//===
+
+console.log("2" === 2);
